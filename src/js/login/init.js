a6b48fb4519f9d84589da62042fb37f7d8173ebc
bill.mark <weiguicheng@foxmail.com>
2018-08-15 10:30:42 +0800

    新增依赖

--- src/js/login/init.js
+++ src/js/login/init.js
@@ -1,10 +1,1 @@
- import render from  './render.js'
- import event from './event.js'
-
-
- window.login = (opts)=>{
-     const container = opts.container;
-     render(container);
-     event()
- }
- 
+import '../common/polyfill.js'
