d6ad100e22bb81c5c9b9578e232c878318ed912a
bill.mark <weiguicheng@foxmail.com>
2018-08-20 18:24:00 +0800

    up

--- src/components/login/event.js
+++ src/components/login/event.js
@@ -1,4 +1,25 @@
 import {getId} from '../../assets/ustis.js'
 export default (opts  =  {}) =>{
   const $loginForm = getId('login-form')
+  const $loginBtn = getId('login-btn')
+  const $remember  = getId('login-rember')
+  const $clearAccount = getId('clear-account')
+  const $clearPassword = getId('clear-password')
+  const $account = getId('login-account')
+  const $password = getId('login-password')
+  const $error = getId('login-error')
+
+  $loginForm.onsubmit = () =>{
+  	//点击登录
+
+  }
+  $account.oninput = () =>{
+
+  }
+  $clearAccount.onclick = () =>{
+
+  }
+  $password.oninput = () =>{
+  	
+  }
 }
